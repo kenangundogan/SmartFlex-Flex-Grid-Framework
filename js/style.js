230d7edfb6295491e0a08f4824b50bbb01080dd8
kenan gündoğan <kenangundogann@gmail.com>
2017-08-19 18:07:35 +0300

    js change 1

--- js/style.js
+++ js/style.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
         var headHeight = $('.head').outerHeight();
 		$('html, body').animate({scrollTop: $('.'+target+'-content').offset().top - headHeight}, 1000);
         activeMenu($(this));
-        console.log($(this));
+        console.log($(this)); 
     }); 
     function activeMenu(obj){
 		$('.menu span').removeClass("active");
